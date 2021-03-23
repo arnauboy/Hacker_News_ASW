29ab8b54e1ef300610f47473f456bbc6c5111550
Ubuntu <ubuntu@ip-172-31-5-170.ec2.internal>
2021-03-23 17:44:24 +0000

    submit only need title + url

--- app/controllers/submit_controller.rb
+++ app/controllers/submit_controller.rb
@@ -3,10 +3,12 @@ class SubmitController < ApplicationController
 
   end
 
-  def create
-    @new = New.new(title: params[:title], url: params[:url], text: params[:text])
 
-    if @new.save
+      
+  def create
+    if !params[:title].blank? && !params[:title].blank?
+        @new = New.new(title: params[:title], url: params[:url])
+        @new.save
       redirect_to newest_path
     else
       render :submit
--- app/models/new.rb
+++ app/models/new.rb
@@ -1,5 +1,4 @@
 class New < ApplicationRecord
   validates :title, length: {minimum: 4}
   validates :url, length: {minimum: 4}
-  validates :text, length: {minimum: 4}
 end
