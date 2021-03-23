f6a1339deb2332bbd7e423c713f1a2f3ee7a5cca
Ubuntu <ubuntu@ip-172-31-0-251.ec2.internal>
2021-03-23 18:20:38 +0000

    Afegit el meu AWS Server com a host pq funcioni

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  #Ferran
+  config.hosts << "9975fb23d3364eaa907827a836b3eecc.vfs.cloud9.us-east-1.amazonaws.com"
+  
 end
