015dfddd5e596875667d4df74e2f7f92a98e6d54
arnauboy <arnauboy@gmail.com>
2021-03-23 13:23:52 +0100

    Creado controlador de newest

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "news#index"
   get "/news", to: "news#index"
+  get "/newest", to: "new#index"
   end
