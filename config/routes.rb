9f29038d648326db93a5e34e48dd927e8602e05e
arnauboy <arnauboy@gmail.com>
2021-03-23 13:31:51 +0100

    Carga de pàgina submit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'submit/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "news#index"
   get "/news", to: "news#index"
   get "/newest", to: "new#index"
+  get "/submit", to: "submit#index"
   end
