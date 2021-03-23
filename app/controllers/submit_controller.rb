07a86a67e15b4d7a84eecaedcbca48d8e85f5e03
arnauboy <arnauboy@gmail.com>
2021-03-23 13:29:02 +0100

    Creado controlador de submit

---
+++ app/controllers/submit_controller.rb
@@ -0,0 +1,5 @@
+class SubmitController < ApplicationController
+  def index
+    render :layout => false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'submit/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "news#index"
   get "/news", to: "news#index"
